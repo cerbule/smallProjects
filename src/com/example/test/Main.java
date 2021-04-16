53d39ef23cd3d552dba464b05e006fb3a196b0d4
cerbule <alindomuta@gmail.com>
2021-04-16 12:13:20 +0300

    Squashing 5 local commits

--- src/com/example/test/Main.java
+++ src/com/example/test/Main.java
@@ -9,5 +9,10 @@ public class Main {
         System.out.println("do");
         System.out.println("a");
         System.out.println("squash");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
     }
 }
