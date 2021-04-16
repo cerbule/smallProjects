5cac9dc0d8e6d82270b978259dc9d126ac1a346e
cerbule <alindomuta@gmail.com>
2021-04-16 13:21:02 +0300

    String and ints

---
+++ src/com/example/test/StringsAndInts.java
@@ -0,0 +1,20 @@
+package com.example.test;
+
+public class StringsAndInts {
+
+    public static String firstField = "rap pup";
+
+
+    public static void main(String[] args) {
+        System.out.println(firstField);
+        String tim;
+        tim = "23";
+        System.out.println(tim);
+
+        int timSalary = 32;
+        int monthly = timSalary * 4;
+        System.out.println(timSalary);
+        System.out.println(monthly);
+    }
+
+}
