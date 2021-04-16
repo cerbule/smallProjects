ea0f4c935ea26835e94e9abca01ac49ef7e8307c
cerbule <alindomuta@gmail.com>
2021-04-16 13:30:17 +0300

    simple concatentation

---
+++ src/com/example/test/Concatenation.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+public class Concatenation {
+
+    public static void main(String[] args) {
+        int timsWeeklySalary = 32;
+        int timsmonthlySalary = timsWeeklySalary * 4;
+        System.out.println("Weekly salaryy: " + timsWeeklySalary);
+        System.out.println("Weekly salaryy: " + timsmonthlySalary);
+
+        System.out.println("Hello " + timsmonthlySalary + " money");
+    }
+
+}
